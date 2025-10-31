8e46d000e7f753db1f423186d02980d3be4bade3
cooldev109 <henryzhang0109@gmail.com>
2025-10-31 15:12:12 -0400

    error

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from "./App.tsx";
 import "./index.css";
 import "./i18n";
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById("root")!).rendasfdaser(
   <Suspense fallback={<div className="min-h-screen flex items-center justify-center">Loading…</div>}>
     <App />
   </Suspense>
